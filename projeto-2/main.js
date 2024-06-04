104f76c31db5f0093cf85f0737f445816901cb18
Júlia Pinheiro <julia_jgp@hotmail.com>
2024-06-03 21:21:02 -0300

    Add files via upload

---
+++ projeto-2/main.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form-atividade');
+const nomes = [];
+const telefones = [];
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNome = document.getElementById('nome');
+    const inputTelefone = document.getElementById('telefone');
+
+    if(nomes.includes(inputNome.value)) {
+        alert(`O contato ${inputNome.value} já foi inserido!`);
+    }
+    else {
+        nomes.push(inputNome.value);
+        telefones.push(parseFloat(inputTelefone.value));
+
+        let linha = '<tr>';
+        linha += `<td>${inputNome.value}</td>`;
+        linha += `<td>${inputTelefone.value}</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+
+    }
+
+    inputNome.value = '';
+    inputTelefone.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
